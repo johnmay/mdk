0907fa29b848aeb1f56363c1102334df89998799
John May <johnmay@ebi.ac.uk>
2011-07-13 14:45:06 +0000

    Create mapping classes to help map tsv/csv files to hash maps
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@130 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/CheMet-IO/src/main/java/uk/ac/ebi/chemet/io/mapping/FileHashMapper.java
@@ -0,0 +1,120 @@
+/**
+ * FileHashMapper.java
+ *
+ * 2011.07.13
+ *
+ * This file is part of the CheMet library
+ *
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.chemet.io.mapping;
+
+import au.com.bytecode.opencsv.CSVReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.log4j.Logger;
+
+/**
+ * @name    FileHashMapper
+ * @date    2011.07.13
+ * @date    $LastChangedDate$ (this version)
+ * @version $Revision$
+ * @author  johnmay
+ * @author  $Author$ (this version)
+ * @brief   Generates a HashMap from a delimited file with specified Key and Value classes
+ *
+ */
+public abstract class FileHashMapper<K , V> {
+
+    private static final Logger logger = Logger.getLogger( FileHashMapper.class );
+    private CSVReader reader;
+    private Integer keyIndex;
+    private Integer valueIndex;
+    private Class<?> keyClass;
+    private Class<?> valuClass;
+
+    /**
+     * @brief Constructor specifying the input reader and the column index of the key
+     *        and the column
+     * @param reader     Instance of CSVReader object (part of OpenCSV library)
+     * @param keyIndex   Index of the column to use as a key
+     * @param valueIndex Index of the column to use as a value
+     */
+    public FileHashMapper( CSVReader reader , Integer keyIndex , Integer valueIndex ) {
+        this.reader = reader;
+        this.keyIndex = keyIndex;
+        this.valueIndex = valueIndex;
+    }
+
+    /**
+     * Builds a hash map by reading a file and using the specified columns for mapping
+     * keys and values
+     * @param  header skip the first line of the file
+     * @return constructed hash map
+     * @throw  IOException
+     */
+    public Map<K , V> getHashMap( boolean header ) throws IOException {
+
+        // if the header is specified skip the first line
+        if ( header ) {
+            String[] headers = reader.readNext();
+        }
+
+        Map<K , V> map = new HashMap<K , V>();
+
+        // reader the file/stream in
+        String[] row = reader.readNext();
+        do {
+
+            // get the key and value objects from the abstract methods
+            K key = parseKey( row[keyIndex] );
+            V value = parseValue( row[valueIndex] );
+
+            if ( map.containsKey( key ) ) {
+                map.put( key , clash( map.get( key ) , value ) );
+            } else {
+                // add to the map
+                map.put( key , value );
+            }
+
+            row = reader.readNext();
+        } while ( row != null );
+
+        return new HashMap<K , V>();
+    }
+
+    /**
+     * Parses a string in to the defined object type
+     * @param keyString
+     * @return
+     */
+    public abstract K parseKey( String keyString );
+    
+    /**
+     * Parses a string in to the defined object type
+     * @param keyString
+     * @return
+     */
+    public abstract V parseValue( String keyString );
+
+    /**
+     * Handles the clashing of keys. Given the old and new values to be stored
+     * the method should decided what todo e.g. discard old, new or merger
+     * @param oldValue
+     * @param newValue
+     * @return
+     */
+    public abstract V clash( V oldValue , V newValue );
+}
---
+++ CheMet/CheMet-IO/src/main/java/uk/ac/ebi/chemet/io/mapping/UniProtECMapper.java
@@ -0,0 +1,84 @@
+/**
+ * UniProtECMapper.java
+ *
+ * 2011.07.13
+ *
+ * This file is part of the CheMet library
+ *
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.chemet.io.mapping;
+
+import au.com.bytecode.opencsv.CSVReader;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.log4j.Logger;
+import uk.ac.ebi.metabolomes.identifier.AbstractIdentifier;
+import uk.ac.ebi.metabolomes.identifier.ECNumber;
+import uk.ac.ebi.metabolomes.identifier.IdentifierFactory;
+import uk.ac.ebi.metabolomes.identifier.UniProtIdentifier;
+
+/**
+ * @name    UniProtECMapper
+ * @date    2011.07.13
+ * @date    $LastChangedDate$ (this version)
+ * @version $Revision$
+ * @author  johnmay
+ * @author  $Author$ (this version)
+ * @brief   Provides mapping of UniProtIds to EC Numbers from a given file
+ *
+ */
+public class UniProtECMapper
+        extends FileHashMapper<UniProtIdentifier , Set<ECNumber>> {
+
+    private static final Logger logger = Logger.getLogger( UniProtECMapper.class );
+
+    /**
+     * Constructor instantiates the UniProt to EC Number mapper with a provided CSVReader
+     * @param reader
+     * @param keyIndex
+     * @param valueIndex
+     */
+    public UniProtECMapper( CSVReader reader , Integer keyIndex , Integer valueIndex ) {
+        super( reader , keyIndex , valueIndex );
+    }
+
+    @Override
+    public UniProtIdentifier parseKey( String keyString ) {
+        AbstractIdentifier id = IdentifierFactory.getIdentifiers( keyString ).get( 0 );
+        if ( id instanceof UniProtIdentifier ) {
+            return ( UniProtIdentifier ) id;
+        }
+        return new UniProtIdentifier( keyString );
+    }
+
+    @Override
+    public Set<ECNumber> parseValue( String valueString ) {
+        Set<ECNumber> ecs = new HashSet<ECNumber>( 4 );
+        // to handle multiple ec numbers first split the string on semi-colon (standard divider)
+        String[] ecStrings = valueString.split( ";" );
+        // add all to the set
+        for ( String ecString : ecStrings ) {
+            ecs.add( new ECNumber( ecString ) );
+        }
+        return ecs;
+    }
+
+    @Override
+    public Set<ECNumber> clash( Set<ECNumber> oldValue ,
+                                Set<ECNumber> newValue ) {
+        oldValue.addAll( newValue );
+        return oldValue;
+    }
+}
