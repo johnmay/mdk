55d5d49a2055320d311b4ee1d38204f74d0f5599
John May <johnmay@ebi.ac.uk>
2011-07-18 13:11:36 +0000

    Created MoleculeBuilder which helps with building CDK IMolecule objects from a variety of inputs
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@161 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/CheMet-Core/src/main/java/uk/ac/ebi/chemet/util/MoleculeBuilder.java
@@ -0,0 +1,105 @@
+/**
+ * MoleculeBuilder.java
+ *
+ * 2011.07.18
+ *
+ * This file is part of the CheMet library
+ *
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.chemet.util;
+
+import java.io.StringReader;
+import org.apache.log4j.Logger;
+import org.openscience.cdk.Molecule;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IMolecule;
+import org.openscience.cdk.io.MDLV2000Reader;
+import org.openscience.cdk.io.MDLV3000Reader;
+import uk.ac.ebi.metabolomes.identifier.InChI;
+
+/**
+ * @name    MoleculeBuilder
+ * @date    2011.07.18
+ * @version $Rev$ : Last Changed $Date$
+ * @author  johnmay
+ * @author  $Author$ (this version)
+ * @brief   ...class description...
+ *
+ */
+public class MoleculeBuilder {
+
+    private static final Logger LOGGER = Logger.getLogger( MoleculeBuilder.class );
+    private static IMolecule template = new Molecule();
+
+    /**
+     * Given and InChI string and a molFile string try and build a IMolecule object
+     * @param inchi
+     * @param molFile
+     * @return
+     */
+    public static IMolecule build( String inchi , String molFile ) {
+
+        // Todo smile
+        IMolecule molecule = null;
+        molecule = buildFromMolFileV2000( new MDLV2000Reader( new StringReader( molFile ) ) );
+        if ( molecule != null ) {
+            return molecule;
+        }
+        molecule = buildFromMolFileV3000( new MDLV3000Reader( new StringReader( molFile ) ) );
+        if ( molecule != null ) {
+            return molecule;
+        }
+        molecule = buildFromInChI( new InChI( inchi ) );
+        return molecule;
+    }
+
+    /**
+     * Builds an IMolecule from an InChI String
+     * @param inchi
+     * @return
+     */
+    public static IMolecule buildFromInChI( InChI inchi ) {
+        return null;
+    }
+
+    /**
+     * Builds an IMolecule from a V2000 mol file
+     * @param reader
+     * @return
+     */
+    public static IMolecule buildFromMolFileV2000( MDLV2000Reader reader ) {
+        try {
+            return reader.read( template );
+        } catch ( CDKException ex ) {
+            // do nothing
+        }
+        return null;
+    }
+
+    /**
+     * Builds an IMolecule from a V3000 mol file
+     * @param reader
+     * @return
+     */
+    public static IMolecule buildFromMolFileV3000( MDLV3000Reader reader ) {
+        try {
+            return reader.read( template );
+        } catch ( CDKException ex ) {
+            // do nothing
+        }
+        return null;
+    }
+    
+}
