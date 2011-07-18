d4e93eee234c6258c5fdaf66455396a6b59270f5
John May <johnmay@ebi.ac.uk>
2011-07-18 10:28:14 +0000

    Created BlastAlignmentColor as a profile to handle alignment colouring of schematics. The class can be used with the LocalAlignmentRenderer to produce a BufferedImage. The image similar to that seen on the BLAST NCBI website.
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@159 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/CheMet-Visualisation/src/main/java/uk/ac/ebi/chemet/visualisation/BlastAlignmentColor.java
@@ -0,0 +1,56 @@
+/**
+ * BlastAlignmentColor.java
+ *
+ * 2011.07.15
+ *
+ * This file is part of the CheMet library
+ *
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.chemet.visualisation;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import org.apache.log4j.Logger;
+import uk.ac.ebi.metabolomes.descriptor.observation.AbstractObservation;
+import uk.ac.ebi.metabolomes.descriptor.observation.sequence.homology.LocalAlignment;
+
+/**
+ * @name    BlastAlignmentColor
+ * @date    2011.07.15
+ * @version $Rev$ : Last Changed $Date$
+ * @author  johnmay
+ * @author  $Author$ (this version)
+ * @brief   ...class description...
+ *
+ */
+public class BlastAlignmentColor
+        extends AbstractAlignmentColor {
+
+    private static final Logger LOGGER = Logger.getLogger( BlastAlignmentColor.class );
+
+    public BlastAlignmentColor() {
+        super( BufferedImage.TYPE_4BYTE_ABGR , Color.WHITE , null , null , Color.LIGHT_GRAY );
+    }
+
+    @Override
+    public Color getMatchColor( AbstractObservation observation ) {
+        if ( observation instanceof LocalAlignment ) {
+            double bitScore = ( ( LocalAlignment ) observation ).getBitScore();
+            return BlastScoreColorRange.getColorForScore( ( int ) bitScore ).getColor();
+        }
+        LOGGER.error("Non local alignment object provided");
+        return Color.RED;
+    }
+}
