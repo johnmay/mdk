d4d213eed1701b84adef385e8cd7c9123b2d098a
John May <johnmay@ebi.ac.uk>
2011-08-03 14:09:14 +0000

    Minor mods
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@264 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/CheMet-IO/src/main/java/uk/ac/ebi/chemet/io/file/MolFileFilter.java
+++ CheMet/CheMet-IO/src/main/java/uk/ac/ebi/chemet/io/file/MolFileFilter.java
@@ -2,20 +2,19 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package uk.ac.ebi.chemet.io.file;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FilenameFilter;
 
 /**
  *
  * @author pmoreno
  */
-public class MolFileFilter implements FileFilter{
+public class MolFileFilter implements FileFilter {
 
-    public boolean accept(File file) {
-        return file.isFile() && file.getName().endsWith(".mol");
+    public boolean accept( File file ) {
+        return file.isFile() && file.getName().endsWith( ".mol" );
     }
-
 }
