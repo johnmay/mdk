5e4897bf8e46574f9fd104532313bfbdf467a32b
John May <johnmay@ebi.ac.uk>
2011-08-03 09:22:15 +0000

    Added MolFileFilter to CheMet-IO
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@240 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/CheMet-IO/src/main/java/uk/ac/ebi/chemet/io/file/MolFileFilter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.ac.ebi.chemet.io.file;
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ *
+ * @author pmoreno
+ */
+public class MolFileFilter implements FileFilter{
+
+    public boolean accept(File file) {
+        return file.isFile() && file.getName().endsWith(".mol");
+    }
+
+}
