ec0bc7d905777bca158e4b3fb6891f7017921228
John May <johnmay@ebi.ac.uk>
2011-08-12 10:41:24 +0000

    Uncommented broken BiochemicalReaction this is now fixed
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@307 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/CheMet-Core/src/main/java/uk/ac/ebi/chemet/entities/reaction/BiochemicalReaction.java
+++ CheMet/CheMet-Core/src/main/java/uk/ac/ebi/chemet/entities/reaction/BiochemicalReaction.java
@@ -1,61 +1,56 @@
-///**
-// * BiochemicalReaction.java
-// *
-// * 2011.08.08
-// *
-// * This file is part of the CheMet library
-// *
-// * The CheMet library is free software: you can redistribute it and/or modify
-// * it under the terms of the GNU Lesser General Public License as published by
-// * the Free Software Foundation, either version 3 of the License, or
-// * (at your option) any later version.
-// *
-// * CheMet is distributed in the hope that it will be useful,
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// * GNU General Public License for more details.
-// *
-// * You should have received a copy of the GNU Lesser General Public License
-// * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
-// */
-//package uk.ac.ebi.chemet.entities.reaction;
-//
-//import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.Comparator;
-//import java.util.List;
-//import org.apache.log4j.Logger;
-//import uk.ac.ebi.metabolomes.core.gene.GeneProduct;
-//
-///**
-// * @name    BiochemicalReaction
-// * @date    2011.08.08
-// * @version $Rev$ : Last Changed $Date$
-// * @author  johnmay
-// * @author  $Author$ (this version)
-// * @brief   Class extends Reaction but adds the option to add a modifier (i.e. enzyme)
-// *
-// */
-//public class BiochemicalReaction<M , S extends Comparable , C extends Comparable>
-//        extends Reaction<M , S , C> {
-//
-//    private static final Logger LOGGER = Logger.getLogger( BiochemicalReaction.class );
-//    private List<GeneProduct> modifiers;
-//
-//    public BiochemicalReaction( Comparator<M> moleculeComparator ) {
-//        this( moleculeComparator , new ArrayList<GeneProduct>() );
-//    }
-//
-//    public BiochemicalReaction( Comparator<M> moleculeComparator , List<GeneProduct> modifiers ) {
-//        super( moleculeComparator );
-//        this.modifiers = modifiers;
-//    }
-//
-//    public List<GeneProduct> getModifiers() {
-//        return Collections.unmodifiableList( modifiers );
-//    }
-//
-//    public void addModifier( GeneProduct modifier ) {
-//        this.modifiers.add( modifier );
-//    }
-//}
+/**
+ * BiochemicalReaction.java
+ *
+ * 2011.08.08
+ *
+ * This file is part of the CheMet library
+ *
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.chemet.entities.reaction;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import org.apache.log4j.Logger;
+import uk.ac.ebi.metabolomes.core.gene.GeneProduct;
+
+/**
+ * @name    BiochemicalReaction
+ * @date    2011.08.08
+ * @version $Rev$ : Last Changed $Date$
+ * @author  johnmay
+ * @author  $Author$ (this version)
+ * @brief   Class extends Reaction but adds the option to add a modifier (i.e. enzyme)
+ *
+ */
+public class BiochemicalReaction<M , S , C>
+        extends Reaction<M , S , C> {
+
+    private static final Logger LOGGER = Logger.getLogger( BiochemicalReaction.class );
+    private List<GeneProduct> modifiers;
+
+    public BiochemicalReaction( List<GeneProduct> modifiers ) {
+        this.modifiers = modifiers;
+    }
+
+    public List<GeneProduct> getModifiers() {
+        return Collections.unmodifiableList( modifiers );
+    }
+
+    public void addModifier( GeneProduct modifier ) {
+        this.modifiers.add( modifier );
+    }
+}
