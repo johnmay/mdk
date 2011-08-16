e03edfa33d08de8b5574cb3b95c90bcca497cdb1
John May <johnmay@ebi.ac.uk>
2011-08-16 16:08:32 +0000

    Added test resources of ATP from KEGG and ATP from ChEBI
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@343 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/CheMet-Core/src/test/java/uk/ac/ebi/chemet/entities/reaction/participant/AtomContainerParticipantTest.java
+++ CheMet/CheMet-Core/src/test/java/uk/ac/ebi/chemet/entities/reaction/participant/AtomContainerParticipantTest.java
@@ -7,6 +7,7 @@ package uk.ac.ebi.chemet.entities.reaction.participant;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openscience.cdk.io.Mol2Reader;
 import static org.junit.Assert.*;
 
 /**
@@ -27,6 +28,14 @@ public class AtomContainerParticipantTest {
     }
 
     @Test
-    public void testCharged() {
+    public void testDiffDatabases() {
+
+        System.out.print( "[Test] Molecules from different databases should be equal..." );
+
+        new Mol2Reader(getClass().getResourceAsStream("ChEBI_15422.mol") );
+
+
+        System.out.println( "Passed" );
+
     }
 }
