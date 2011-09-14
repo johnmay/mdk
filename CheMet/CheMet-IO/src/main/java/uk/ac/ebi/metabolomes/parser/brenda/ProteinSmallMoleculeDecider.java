a40ad191c7dc64e5fc719da6db9783c4689e9d1f
Pablo Moreno <pablo.a.moreno@gmail.com>
2011-09-14 10:28:39 +0000

    Changed all the regexps so that the patterns are only compiled once, and not every time the accessible static method is invoked.
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@399 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/CheMet-IO/src/main/java/uk/ac/ebi/metabolomes/parser/brenda/ProteinSmallMoleculeDecider.java
+++ CheMet/CheMet-IO/src/main/java/uk/ac/ebi/metabolomes/parser/brenda/ProteinSmallMoleculeDecider.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package uk.ac.ebi.metabolomes.parser.brenda;
 
 import java.util.regex.Pattern;
@@ -12,65 +11,153 @@ import java.util.regex.Pattern;
  * @author pmoreno
  */
 public class ProteinSmallMoleculeDecider {
-
+    private static final Pattern ase_end_Pattern = Pattern.compile("(?i)ase$");
+    private static final Pattern ase_sep_Pattern = Pattern.compile("(?i)ase[ \\]] ");
+    private static final Pattern brcaPattern = Pattern.compile("(?i)BRCA");
+    private static final Pattern calmodulinPattern = Pattern.compile("(?i)calmodulin");
+    private static final Pattern caseinPattern = Pattern.compile("(?i)casein");
+    private static final Pattern caspasePattern = Pattern.compile("(?i)caspase");
+    private static final Pattern cateninPattern = Pattern.compile("(?i)catenin");
+    private static final Pattern channelPattern = Pattern.compile("(?i)channel");
+    private static final Pattern claudinPattern = Pattern.compile("(?i)claudin");
+    private static final Pattern coenzymeAntiPattern = Pattern.compile("(?i)coenzyme");
+    private static final Pattern cofactorAntiPattern = Pattern.compile("(?i)cofactor");
+    private static final Pattern collagenPattern = Pattern.compile("(?i)collagen");
+    private static final Pattern cytochromePattern = Pattern.compile("(?i)cytochrome");
+    private static final Pattern doxinPattern = Pattern.compile("(?i)doxin");
+    private static final Pattern elastinPattern = Pattern.compile("(?i)elastin");
+    private static final Pattern enzymePattern = Pattern.compile("(?i)enzyme");
+    private static final Pattern factorPattern = Pattern.compile("(?i)factor");
+    private static final Pattern fibrinPattern = Pattern.compile("(?i)fibrin");
+    private static final Pattern fibronectinPattern = Pattern.compile("(?i)fibronect");
+    private static final Pattern gelsolinPattern = Pattern.compile("(?i)gelsolin");
+    private static final Pattern globinPattern = Pattern.compile("(?i)globin");
+    private static final Pattern glycogeninPattern = Pattern.compile("(?i)glycogenin");
+    private static final Pattern histonePattern = Pattern.compile("(?i)istone");
+    private static final Pattern kinasePattern = Pattern.compile("(?i)kinase");
+    private static final Pattern kininogenPattern = Pattern.compile("(?i)kininogen");
+    private static final Pattern myosinPattern = Pattern.compile("(?i)myosin");
+    private static final Pattern paxilinPattern = Pattern.compile("(?i)paxilin");
+    private static final Pattern phosphorylatedPattern = Pattern.compile("(?i)phosphorylated");
+    private static final Pattern plasminPattern = Pattern.compile("(?i)plasmin");
+    private static final Pattern protaminePattern = Pattern.compile("(?i)protamine");
+    private static final Pattern proteasPattern = Pattern.compile("(?i)proteas");
+    private static final Pattern proteinPattern = Pattern.compile("(?i)protein");
+    private static final Pattern proteoglycanPattern = Pattern.compile("(?i)proteglycan");
+    private static final Pattern pyridoxinePattern = Pattern.compile("(?i)pyridoxine");
+    private static final Pattern receptorPattern = Pattern.compile("(?i)receptor");
+    private static final Pattern rhodaminPattern = Pattern.compile("(?i)rhodamin");
+    private static final Pattern rhodopsinPattern = Pattern.compile("(?i)rhodopsin");
+    private static final Pattern septinPattern = Pattern.compile("(?i)septin");
+    private static final Pattern smadPattern = Pattern.compile("(?i)smad");
+    private static final Pattern synthasePattern = Pattern.compile("(?i)synthase");
+    private static final Pattern thrombinPattern = Pattern.compile("(?i)thrombin");
+    private static final Pattern thrombospondinPattern = Pattern.compile("(?i)thrombospondin");
+    private static final Pattern transferrinPattern = Pattern.compile("(?i)transferrin");
+    private static final Pattern tubulinPattern = Pattern.compile("(?i)tubulin");
+    private static final Pattern vimentinPattern = Pattern.compile("(?i)vimentin");
+    
     public static boolean isProteinName(String name) {
-        if(Pattern.compile("(?i)kinase").matcher(name).find()) return true;
-        if(Pattern.compile("(?i)protein").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)istone").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)doxin").matcher(name).find()) {
-    if(Pattern.compile("(?i)pyridoxine").matcher(name).find())
+        if (kinasePattern.matcher(name).find()) {
+            return true;
+        }
+        if (proteinPattern.matcher(name).find()) {
+            return true;
+        } else if (histonePattern.matcher(name).find()) {
+            return true;
+        } else if (doxinPattern.matcher(name).find()) {
+            if (pyridoxinePattern.matcher(name).find()) {
+                return false;
+            }
+            return true;
+        } else if (caseinPattern.matcher(name).find()) {
+            return true;
+        } else if (collagenPattern.matcher(name).find()) {
+            return true;
+        } else if (elastinPattern.matcher(name).find()) {
+            return true;
+        } //else if(Pattern.compile("(?i)in$").matcher(name).find()) return true;
+        //else if(Pattern.compile("(?i)in ").matcher(name).find()) return true;
+        else if (receptorPattern.matcher(name).find()) {
+            return true;
+        } else if (channelPattern.matcher(name).find()) {
+            return true;
+        } else if (brcaPattern.matcher(name).find()) {
+            return true;
+        } else if (calmodulinPattern.matcher(name).find()) {
+            return true;
+        } else if (claudinPattern.matcher(name).find()) {
+            return true;
+        } else if (enzymePattern.matcher(name).find()) {
+            if (coenzymeAntiPattern.matcher(name).find()) {
+                return false;
+            }
+            return true;
+        } else if (factorPattern.matcher(name).find()) {
+            if (cofactorAntiPattern.matcher(name).find()) {
+                return false;
+            }
+            return true;
+        } else if (cytochromePattern.matcher(name).find()) {
+            return true;
+        } else if (fibrinPattern.matcher(name).find()) {
+            return true;
+        } else if (fibronectinPattern.matcher(name).find()) {
+            return true;
+        } else if (kininogenPattern.matcher(name).find()) {
+            return true;
+        } else if (ase_sep_Pattern.matcher(name).find()) {
+            return true;
+        } else if (ase_end_Pattern.matcher(name).find()) {
+            return true;
+        } else if (synthasePattern.matcher(name).find()) {
+            return true;
+        } else if (glycogeninPattern.matcher(name).find()) {
+            return true;
+        // repeated, present before
+        //} else if (Pattern.compile("(?i)receptor").matcher(name).find()) {
+        //    return true;
+        } else if (globinPattern.matcher(name).find()) {
+            return true;
+        } else if (myosinPattern.matcher(name).find()) {
+            return true;
+        } else if (paxilinPattern.matcher(name).find()) {
+            return true;
+        } else if (gelsolinPattern.matcher(name).find()) {
+            return true;
+        } else if (cateninPattern.matcher(name).find()) {
+            return true;
+        } else if (phosphorylatedPattern.matcher(name).find()) {
+            return true;
+        } else if (plasminPattern.matcher(name).find()) {
+            return true;
+        } else if (caspasePattern.matcher(name).find()) {
+            return true;
+        } else if (proteasPattern.matcher(name).find()) {
+            return true;
+        } else if (thrombinPattern.matcher(name).find()) {
+            return true;
+        } else if (rhodaminPattern.matcher(name).find()) {
+            return true;
+        } else if (smadPattern.matcher(name).find()) {
+            return true;
+        } else if (septinPattern.matcher(name).find()) {
+            return true;
+        } else if (thrombospondinPattern.matcher(name).find()) {
+            return true;
+        } else if (vimentinPattern.matcher(name).find()) {
+            return true;
+        } else if (tubulinPattern.matcher(name).find()) {
+            return true;
+        } else if (rhodopsinPattern.matcher(name).find()) {
+            return true;
+        } else if (protaminePattern.matcher(name).find()) {
+            return true;
+        } else if (transferrinPattern.matcher(name).find()) {
+            return true;
+        } else if (proteoglycanPattern.matcher(name).find()) {
+            return true;
+        }
         return false;
-    return true;
-}
-else if(Pattern.compile("(?i)casein").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)collagen").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)elastin").matcher(name).find()) return true;
-//else if(Pattern.compile("(?i)in$").matcher(name).find()) return true;
-//else if(Pattern.compile("(?i)in ").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)receptor").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)channel").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)BRCA").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)calmodulin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)claudin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)enzyme").matcher(name).find()) {
-    if(Pattern.compile("(?i)coenzyme").matcher(name).find())
-        return false;
-    return true;
-}
-else if(Pattern.compile("(?i)factor").matcher(name).find()) {
- if(Pattern.compile("(?i)cofactor").matcher(name).find())
-     return false;
- return true;
-}
-else if(Pattern.compile("(?i)cytochrome").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)fibrin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)fibronect").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)kininogen").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)ase[ \\]] ").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)ase$").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)synthase").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)glycogenin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)receptor").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)globin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)myosin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)paxilin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)gelsolin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)catenin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)phosphorylated").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)plasmin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)caspase").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)proteas").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)thrombin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)rhodamin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)smad").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)septin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)thrombospondin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)vimentin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)tubulin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)rhodopsin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)protamine").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)transferrin").matcher(name).find()) return true;
-else if(Pattern.compile("(?i)proteglycan").matcher(name).find()) return true;
-return false;
     }
 }
