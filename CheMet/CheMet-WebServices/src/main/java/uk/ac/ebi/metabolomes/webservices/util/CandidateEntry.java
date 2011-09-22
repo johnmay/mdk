8cdff9c6fcf755217a1a0146094c4db4a30e2071
John May <johnmay@ebi.ac.uk>
2011-09-22 14:29:46 +0000

    Added constructor to assisted in factory methods. Also update to used Guava utils (Objects) and removed getter/setter javadoc.
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@459 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/CheMet-WebServices/src/main/java/uk/ac/ebi/metabolomes/webservices/util/CandidateEntry.java
+++ CheMet/CheMet-WebServices/src/main/java/uk/ac/ebi/metabolomes/webservices/util/CandidateEntry.java
@@ -1,142 +1,196 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+
+/**
+ * PreparsedMetabolite.java
+ *
+ * 2011.08.15
+ *
+ * This file is part of the CheMet library
+ *
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.ac.ebi.metabolomes.webservices.util;
 
+import static com.google.common.base.Objects.hashCode;
+import static com.google.common.base.Objects.equal;
+
+
 /**
  *
- * @author pmoreno
+ * A Candidate entry describes a paired identifier/description with a distance and comment. The
+ * distance can be calculated in numerous ways (see: {@see ChemicalNameEntryDecider} and
+ * {@see EntryDecider}).
+ *
+ * @author Pablo Moreno
+ * @author John May 
+ * 
  */
-public class CandidateEntry implements Comparable<CandidateEntry> {
+public class CandidateEntry
+  implements Comparable<CandidateEntry> {
 
     private String id;
-    private String desc;
+    private String description;
     private Integer distance;
     private String comment;
 
 
-    /**
-     * @brief Default constructor
-     */
     public CandidateEntry() {
     }
 
+
     /**
-     * @brief Convenience constructor
+     * Construct a candidate entry given all possible attributes
+     * 
      * @param id        Candidate Identifier
      * @param desc      Description of the candidate (e.g. compound name)
-     * @param distance  The Levenshtein distance (number of changes needed), as calculated by Apache Lang StringUtils
+     * @param distance  The distance between the candidate and the query (not-stored in class)
      * @param comment   Additional comments on the candidate
+     * 
      */
-    public CandidateEntry( String id , String desc , Integer distance , String comment ) {
+    public CandidateEntry(String id, String desc, Integer distance, String comment) {
         this.id = id;
-        this.desc = desc;
+        this.description = desc;
         this.distance = distance;
         this.comment = comment;
     }
 
-    /**
-     * @return the id
-     */
+
     public String getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
-    public void setId( String id ) {
+
+    public void setId(String id) {
         this.id = id;
     }
 
-    /**
-     * @return the desc
-     */
+
     public String getDesc() {
-        return desc;
+        return description;
     }
 
-    /**
-     * @param desc the desc to set
-     */
-    public void setDesc( String desc ) {
-        this.desc = desc;
+
+    public String getDescription() {
+        return description;
     }
 
+
+    public void setDesc(String desc) {
+        this.description = desc;
+    }
+
+
     /**
-     * @return the distance
+     * @return the distance of this candidate
      */
     public Integer getDistance() {
         return distance;
     }
 
+
     /**
      * @param distance the distance to set
      */
-    public void setDistance( Integer distance ) {
+    public void setDistance(Integer distance) {
         this.distance = distance;
     }
 
+
+    /**
+     * 
+     * Compares two candidates on their set distance. If either distance is null the objects are
+     * considered equal '0'
+     *
+     */
     @Override
-    public int compareTo( CandidateEntry o ) {
-        if ( distance == null || o.distance == null ) {
+    public int compareTo(CandidateEntry o) {
+        if( distance == null || o.distance == null ) {
             return 0;
         }
-        return this.distance.compareTo( o.distance );
+        return this.distance.compareTo(o.distance);
     }
 
-    public void setComment( String comment ) {
+
+    public void setComment(String comment) {
         this.comment = comment;
     }
 
-    /**
-     * @return the comment
-     */
+
     public String getComment() {
         return comment;
     }
 
 
-
+    /**
+     * @inheritDoc
+     */
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 53 * hash + ( this.id != null ? this.id.hashCode() : 0 );
-        hash = 53 * hash + ( this.desc != null ? this.desc.hashCode() : 0 );
-        hash = 53 * hash + ( this.distance != null ? this.distance.hashCode() : 0 );
-        hash = 53 * hash + ( this.comment != null ? this.comment.hashCode() : 0 );
-        return hash;
+        return hashCode(id, description, distance, comment);
     }
 
+
+    /**
+     * @inheritDoc
+     */
     @Override
-    public boolean equals( Object obj ) {
-        if ( obj == null ) {
+    public boolean equals(Object obj) {
+        if( obj == null ) {
             return false;
         }
-        if ( getClass() != obj.getClass() ) {
+        if( getClass() != obj.getClass() ) {
             return false;
         }
-        final CandidateEntry other = ( CandidateEntry ) obj;
-        if ( ( this.id == null ) ? ( other.id != null ) : !this.id.equals( other.id ) ) {
+        final CandidateEntry other = (CandidateEntry) obj;
+
+        if( !equal(this.id, other.id) ) {
             return false;
         }
-        if ( ( this.desc == null ) ? ( other.desc != null ) : !this.desc.equals( other.desc ) ) {
+        if( !equal(this.description, other.description) ) {
             return false;
         }
-        if ( this.distance != other.distance && ( this.distance == null || !this.distance.equals( other.distance ) ) ) {
+        if( !equal(this.distance, other.distance) ) {
             return false;
         }
-        if ( ( this.comment == null ) ? ( other.comment != null ) : !this.comment.equals( other.comment ) ) {
+        if( !equal(this.comment, other.comment) ) {
             return false;
         }
         return true;
     }
 
 
-
+    /**
+     * @inheritDoc
+     */
     @Override
-    public String toString () {
-        return getId() + ": " + getDesc() + " d=" + getDistance();
+    public String toString() {
+        StringBuilder sb = new StringBuilder(30);
+        return sb.append(id).append(": ").append(this.description).append(" d=").append(
+          this.distance).toString();
     }
+
+
+    /**
+     *
+     * Clones the current candidate id and description with a new distance and comment. This
+     * methods is primarily used in factory methods
+     *
+     * @return A new candidate entry
+     * 
+     */
+    public CandidateEntry newInstance(Integer distance, String comment) {
+        return new CandidateEntry(this.id, this.description, distance, comment);
+    }
+
+
 }
