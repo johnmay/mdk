5d9cdcfce70c6bd2258cf3c8a137f8e9ad102b0e
John May <johnmay@ebi.ac.uk>
2011-09-27 13:58:18 +0000

    Added test for un-parsable ec number and reaction classification
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@477 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/Resource/src/test/java/uk/ac/ebi/resource/classification/ECNumberTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uk.ac.ebi.resource.classification;
+
+import com.sun.source.tree.AssertTree;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import uk.ac.ebi.interfaces.Identifier;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author johnmay
+ */
+public class ECNumberTest {
+
+    public ECNumberTest() {
+    }
+
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+
+    @Test
+    public void testUnderterminedParsing() {
+        Identifier id = new ECNumber("EC-Undertermined");
+        assertEquals("-.-.-.-", id.getAccession());
+    }
+    
+    @Test
+    public void testNormalParsing() {
+        Identifier id = new ECNumber("EC-5.3.1.23");
+        assertEquals("5.3.1.23", id.getAccession());
+    }
+
+
+}
+
