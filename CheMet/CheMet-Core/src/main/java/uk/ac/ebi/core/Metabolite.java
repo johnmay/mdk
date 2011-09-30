a82f1355a3ad698b1ecdd510b13eea4ddcacda8f
John May <johnmay@ebi.ac.uk>
2011-09-30 12:42:47 +0000

    renamed method
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@495 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/CheMet-Core/src/main/java/uk/ac/ebi/core/Metabolite.java
+++ CheMet/CheMet-Core/src/main/java/uk/ac/ebi/core/Metabolite.java
@@ -124,7 +124,7 @@ public class Metabolite
      * @return
      * 
      */
-    public ChemicalStructure getFirstChemicalStructureAnnotation() {
+    public ChemicalStructure getFirstChemicalStructure() {
         return getChemicalStructures().iterator().next();
     }
 
