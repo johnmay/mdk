ba1f7c5ae735e99a969545f2fe0e34ebbd3e6797
John May <johnmay@ebi.ac.uk>
2011-10-19 10:32:03 +0000

    Adding new classes
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@671 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/CheMet-IO/src/main/java/uk/ac/ebi/io/remote/AbstrastRemoteResource.java
@@ -0,0 +1,94 @@
+/**
+ * AbstrastRemoteResource.java
+ *
+ * 2011.10.15
+ *
+ * This file is part of the CheMet library
+ * 
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.io.remote;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import org.apache.log4j.Logger;
+
+/**
+ *          AbstrastRemoteResource - 2011.10.15 <br>
+ *          An abstract class implementing most of default 
+ * @version $Rev$ : Last Changed $Date$
+ * @author  johnmay
+ * @author  $Author$ (this version)
+ */
+public abstract class AbstrastRemoteResource implements RemoteResource {
+
+    private static final Logger LOGGER = Logger.getLogger(AbstrastRemoteResource.class);
+    private URL remote;
+    private File local;
+    private Date lastUpdated = new Date();
+
+    public AbstrastRemoteResource(URL remote, File local) {
+        this.remote = remote;
+        this.local = local;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public Date getLastUpdated() {
+        lastUpdated.setTime(local.exists() ? local.lastModified() : 0);
+        return lastUpdated;
+    }
+
+    public URL getRemote() {
+        return remote;
+    }
+
+    public File getLocal() {
+        return local;
+    }
+
+    public long getLocalSize() {
+        return local.length();
+    }
+
+    public long getRemoteSize() {
+        try {
+            long length = (long) remote.openConnection().getContentLength();
+            remote.openStream().close(); // attempt to free resources
+            return length;
+        } catch (IOException ex) {
+            LOGGER.info("Unable to determine length of remote tile");
+        }
+        return 0l;
+    }
+
+    /**
+     * Displays the file size nicely
+     * @param size
+     * @return
+     */
+    public static String readableFileSize(long size) {
+        if (size <= 0) {
+            return "0";
+        }
+        final String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
+}
---
+++ CheMet/CheMet-IO/src/main/java/uk/ac/ebi/metabolomes/execs/ENAXMLtoFasta.java
@@ -0,0 +1,93 @@
+/**
+ * ENAXMLtoFasta.java
+ *
+ * 2011.10.18
+ *
+ * This file is part of the CheMet library
+ * 
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.metabolomes.execs;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.apache.commons.cli.Option;
+import org.apache.log4j.Logger;
+import org.biojava3.core.sequence.ProteinSequence;
+import org.biojava3.core.sequence.io.FastaWriterHelper;
+import uk.ac.ebi.core.ProteinProduct;
+import uk.ac.ebi.interfaces.GeneProduct;
+import uk.ac.ebi.io.xml.ENAXMLReader;
+
+/**
+ *          ENAXMLtoFasta - 2011.10.18 <br>
+ *          Class description
+ * @version $Rev$ : Last Changed $Date$
+ * @author  johnmay
+ * @author  $Author$ (this version)
+ */
+public class ENAXMLtoFasta extends CommandLineMain {
+
+    private static final Logger LOGGER = Logger.getLogger(ENAXMLtoFasta.class);
+
+    public static void main(String[] args) {
+        new ENAXMLtoFasta(args).process();
+    }
+
+    public ENAXMLtoFasta(String[] args) {
+        super(args);
+    }
+
+    @Override
+    public void setupOptions() {
+        add(new Option("x", "xml", true, "ENA XML File"));
+        add(new Option("o", "output", true, "Output fasta file"));
+    }
+
+    @Override
+    public void process() {
+        try {
+
+            if (!hasOption("x") || !hasOption("o")) {
+                printHelp();
+                System.exit(0);
+            }
+
+            Collection<GeneProduct> products = new ArrayList();
+
+            String[] xmlPaths = getCommandLine().getOptionValues("x");
+            for (String path : xmlPaths) {
+                ENAXMLReader reader = new ENAXMLReader(new FileInputStream(path));
+                products.addAll(reader.getProducts());
+            }
+
+            Collection<ProteinSequence> sequences = new ArrayList();
+            for (GeneProduct product : products) {
+                if (product instanceof ProteinProduct) {
+                    ProteinSequence sequence = (ProteinSequence) product.getSequence();
+                    sequence.setOriginalHeader(product.getAbbreviation() + " " + product.getName());
+                    sequences.add(sequence);
+                }
+            }
+
+
+            FastaWriterHelper.writeProteinSequence(getFileOption("o"), sequences);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+}
