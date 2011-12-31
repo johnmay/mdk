6aaf090ceee0e6977a4127ee415255f245f2cacf
Pablo Moreno <pablo.a.moreno@gmail.com>
2011-12-31 13:36:45 +0000

    PubChemConnectivityService ready.
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1119 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/CheMet-IO/src/test/java/uk/ac/ebi/io/service/PubChemCompoundMoleculeConnectivityServiceTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.ebi.io.service;
+
+import java.util.Collection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import uk.ac.ebi.resource.chemical.PubChemCompoundIdentifier;
+
+/**
+ *
+ * @author pmoreno
+ */
+public class PubChemCompoundMoleculeConnectivityServiceTest {
+    
+    public PubChemCompoundMoleculeConnectivityServiceTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getInstance method, of class PubChemCompoundMoleculeConnectivityService.
+     *
+    @Test
+    public void testGetInstance() {
+        System.out.println("getInstance");
+        PubChemCompoundMoleculeConnectivityService expResult = null;
+        PubChemCompoundMoleculeConnectivityService result = PubChemCompoundMoleculeConnectivityService.getInstance();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEntriesWithConnectivity method, of class PubChemCompoundMoleculeConnectivityService.
+     */
+    @Test
+    public void testGetEntriesWithConnectivity() {
+        System.out.println("getEntriesWithConnectivity");
+        String connectivity = "InChI=1S/C6%O6/c7-1-2-3(8)4(9)5(10)6(11)12-2/";
+        PubChemCompoundMoleculeConnectivityService instance = PubChemCompoundMoleculeConnectivityService.getInstance();
+        Collection<PubChemCompoundIdentifier> result = instance.getEntriesWithConnectivity(connectivity);
+        assertNotNull(result);
+        assertTrue(result.size()>0);
+        for (PubChemCompoundIdentifier identifier : result) {
+            assertNotNull(identifier);
+            assertNotNull(identifier.getAccession());
+            System.out.println("ID: "+identifier.getAccession());
+        }
+    }
+
+    /**
+     * Test of getIdentifier method, of class PubChemCompoundMoleculeConnectivityService.
+     *
+    @Test
+    public void testGetIdentifier() {
+        System.out.println("getIdentifier");
+        PubChemCompoundMoleculeConnectivityService instance = null;
+        PubChemCompoundIdentifier expResult = null;
+        PubChemCompoundIdentifier result = instance.getIdentifier();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }*/
+}
