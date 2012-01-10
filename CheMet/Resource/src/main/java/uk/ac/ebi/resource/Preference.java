a1922307e8e7b087afbb8b0f5db995ae43b074df
John May <johnmay@ebi.ac.uk>
2012-01-10 13:09:11 +0000

    Formating
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1158 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/Resource/src/main/java/uk/ac/ebi/resource/Preference.java
+++ CheMet/Resource/src/main/java/uk/ac/ebi/resource/Preference.java
@@ -9,33 +9,51 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.prefs.Preferences;
 
+
 /**
  * Enumeration organises user and system preferences
+ *
  * @author johnmay
  */
 public enum Preference {
 
-    /* TEST */
+    /*
+     * TEST
+     */
     LIST_TEST(Type.TEST, Access.USER, new ArrayList<String>()),
-    /* IO */
+    /*
+     * IO
+     */
     BUFFER_SIZE(Type.IO, Access.USER, 2048),
     RECENT_FILES(Type.IO, Access.USER, ""),
-    /* Resource */
+    /*
+     * Resource
+     */
     CHEMICAL_IDENTIFIER_FORMAT(Type.IDENTIFIER, Access.USER, "M/%05d"),
     PROTEIN_IDENTIFIER_FORMAT(Type.IDENTIFIER, Access.USER, "P/%05d"),
     GENE_IDENTIFIER_FORMAT(Type.IDENTIFIER, Access.USER, "G/%05d"),
     REACTION_IDENTIFIER_FORMAT(Type.IDENTIFIER, Access.USER, "R/%d"),
-    /* Tool */
+    /*
+     * Tool
+     */
     CPLEX_PATH(Type.TOOL, Access.USER, ""),
     BLASTP_PATH(Type.TOOL, Access.USER, ""),
     BLASTP_VERSION(Type.TOOL, Access.USER, "");
+
     public static final String LIST_SEPARATOR_SPLIT = "(?<!\\\\);";
+
     public static final String LIST_SEPARATOR = ";";
+
     public static final String LIST_SEPARATOR_ESCAPED = "\\\\;";
+
     private String key;
+
     private Type type;
+
     private Access access;
+
     private Object defaultValue;
+
 
     private Preference(Type type, Access access, Object defaultValue) {
         this.type = type;
@@ -44,17 +62,21 @@ public enum Preference {
         this.key = type + "." + name();
     }
 
+
     /**
      * Access the string value of the preference
-     * @return 
+     *
+     * @return
      */
     public String get() {
         return access.getPreferences().get(key, defaultValue.toString());
     }
 
+
     /**
      * Access a list of string for the preference
-     * @return 
+     *
+     * @return
      */
     public List<String> getList() {
         String rawValue = get();
@@ -65,9 +87,11 @@ public enum Preference {
         return values;
     }
 
+
     /**
      * Access a integer value from the preference
-     * @return 
+     *
+     * @return
      */
     public int getInteger() {
         if (!(defaultValue instanceof Integer)) {
@@ -76,9 +100,11 @@ public enum Preference {
         return access.getPreferences().getInt(key, (Integer) defaultValue);
     }
 
+
     /**
      * Access a double value from the preference
-     * @return 
+     *
+     * @return
      */
     public double getDouble() {
         if (!(defaultValue instanceof Double)) {
@@ -87,13 +113,16 @@ public enum Preference {
         return access.getPreferences().getDouble(key, (Double) defaultValue);
     }
 
+
     public void put(String value) {
         access.getPreferences().put(key, value);
     }
 
+
     /**
      * Puts a list into preferences
-     * @param values 
+     *
+     * @param values
      */
     public void putList(List<String> values) {
         StringBuilder sb = new StringBuilder(values.size() * 14);
@@ -106,28 +135,35 @@ public enum Preference {
         put(sb.toString());
     }
 
+
     public void putInteger(int value) {
         access.getPreferences().putInt(key, value);
     }
+
 
     public void putDouble(int value) {
         access.getPreferences().putDouble(key, value);
     }
 
+
     private enum Access {
 
         USER(Preferences.userNodeForPackage(Preference.class)),
         SYSTEM(Preferences.systemNodeForPackage(Preference.class));
+
         private final Preferences preferences;
+
 
         private Access(Preferences preferences) {
             this.preferences = preferences;
         }
 
+
         public Preferences getPreferences() {
             return preferences;
         }
     };
+
 
     private enum Type {
 
