83d051cad6cb235e62e05ab552dcc321a19bc66c
John May <johnmay@ebi.ac.uk>
2012-02-06 09:11:26 +0000

    Minor javadoc changes
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1360 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/interface/src/main/java/uk/ac/ebi/interfaces/InterfaceUtil.java
+++ CheMet/interface/src/main/java/uk/ac/ebi/interfaces/InterfaceUtil.java
@@ -21,7 +21,6 @@ package uk.ac.ebi.interfaces;
  * You should have received a copy of the GNU Lesser General Public License
  * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
  */
-import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.log4j.Logger;
@@ -35,7 +34,7 @@ import uk.ac.ebi.interfaces.entities.Entity;
  * @author  johnmay
  * @author  $Author$ (this version)
  *
- *          Class description
+ *          Utilities class for interfaces
  *
  */
 public class InterfaceUtil {
