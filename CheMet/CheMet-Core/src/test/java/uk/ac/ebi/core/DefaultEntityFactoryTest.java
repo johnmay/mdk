5d81a000425974c12ca61375dddb6601c8b2c08e
John May <johnmay@ebi.ac.uk>
2012-02-06 09:28:29 +0000

    Added unit test for getEntityClass
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1365 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/CheMet-Core/src/test/java/uk/ac/ebi/core/DefaultEntityFactoryTest.java
+++ CheMet/CheMet-Core/src/test/java/uk/ac/ebi/core/DefaultEntityFactoryTest.java
@@ -37,12 +37,23 @@ public class DefaultEntityFactoryTest {
     public void testNewInstance() {
 
         EntityFactory factory = DefaultEntityFactory.getInstance();
-        
+
         Gene gene = factory.newInstance(Gene.class);
         Assert.assertEquals(GeneImplementation.class, gene.getClass());
-        
+
         Metabolite metabolite = factory.newInstance(Metabolite.class);
         Assert.assertEquals(uk.ac.ebi.core.MetaboliteImplementation.class, metabolite.getClass());
+
+    }
+
+
+    @Test
+    public void testGetEntityClass() {
+        
+        EntityFactory factory = DefaultEntityFactory.getInstance();
+
+        Assert.assertEquals(Gene.class, factory.getEntityClass(GeneImplementation.class));
+        Assert.assertEquals(Metabolite.class, factory.getEntityClass(MetaboliteImplementation.class));
 
     }
 }
