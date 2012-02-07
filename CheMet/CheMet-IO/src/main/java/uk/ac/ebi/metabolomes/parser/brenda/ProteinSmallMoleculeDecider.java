811a9e8cd90ea77dcf798635b76f255030ec393a
Pablo Moreno <pablo.a.moreno@gmail.com>
2012-02-07 23:19:43 +0000

    Additional regexp for Protein Name Decider.
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1397 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/CheMet-IO/src/main/java/uk/ac/ebi/metabolomes/parser/brenda/ProteinSmallMoleculeDecider.java
+++ CheMet/CheMet-IO/src/main/java/uk/ac/ebi/metabolomes/parser/brenda/ProteinSmallMoleculeDecider.java
@@ -11,9 +11,12 @@ import java.util.regex.Pattern;
  * @author pmoreno
  */
 public class ProteinSmallMoleculeDecider {
+    
     private static final Pattern ase_end_Pattern = Pattern.compile("(?i)ase$");
     private static final Pattern ase_sep_Pattern = Pattern.compile("(?i)ase[ \\]] ");
     private static final Pattern acpPattern = Pattern.compile("(?i)\\[acp\\]"); // acp : acyl carrier protein
+    private static final Pattern acpPattern2 = Pattern.compile("(?i)\\-ACPs{0,1}$"); // acp : acyl carrier protein
+    private static final Pattern acpPattern3 = Pattern.compile("(?)\\-ACP\\-"); // acp : acyl carrier protein
     private static final Pattern brcaPattern = Pattern.compile("(?i)BRCA");
     private static final Pattern eIF5aPrecPattern = Pattern.compile("(?i)eIF5A-precursor");
     private static final Pattern calmodulinPattern = Pattern.compile("(?i)calmodulin");
@@ -34,7 +37,7 @@ public class ProteinSmallMoleculeDecider {
     private static final Pattern factorPattern = Pattern.compile("(?i)factor");
     private static final Pattern fibrinPattern = Pattern.compile("(?i)fibrin");
     private static final Pattern fibronectinPattern = Pattern.compile("(?i)fibronect");
-    private static final Pattern gelsolinPattern = Pattern.compile("(?i)gelsolin");
+    private static final Pattern gelsolinPattern = Pattern.compile("(?i)gelsol+in");
     private static final Pattern globinPattern = Pattern.compile("(?i)globin");
     private static final Pattern glycogeninPattern = Pattern.compile("(?i)glycogenin");
     private static final Pattern histonePattern = Pattern.compile("(?i)istone");
@@ -42,7 +45,7 @@ public class ProteinSmallMoleculeDecider {
     private static final Pattern kininogenPattern = Pattern.compile("(?i)kininogen");
     private static final Pattern ligasePattern = Pattern.compile("(?i)ligase");
     private static final Pattern myosinPattern = Pattern.compile("(?i)myosin");
-    private static final Pattern paxilinPattern = Pattern.compile("(?i)paxilin");
+    private static final Pattern paxilinPattern = Pattern.compile("(?i)pax+il+in");
     private static final Pattern phosphorylatedPattern = Pattern.compile("(?i)phosphorylated");
     // [phosphorylase a] still goes undetected
     private static final Pattern phosphorylasePattern = Pattern.compile("(?i)phosphorylase");
@@ -64,6 +67,30 @@ public class ProteinSmallMoleculeDecider {
     private static final Pattern transferasePattern = Pattern.compile("(?i)transferase");
     private static final Pattern tubulinPattern = Pattern.compile("(?i)tubulin");
     private static final Pattern vimentinPattern = Pattern.compile("(?i)vimentin");
+    private static final Pattern cohesinPattern = Pattern.compile("(?i)cohesin");
+    private static final Pattern angiotensinPattern = Pattern.compile("(?i)angiotensin");
+    private static final Pattern cadherinPattern = Pattern.compile("(?i)cadherin");
+    private static final Pattern domainsEndingPattern = Pattern.compile("(?i)-domains{0,1}$");
+    private static final Pattern transporterPattern = Pattern.compile("(?i)transporter");
+    private static final Pattern lipasePattern = Pattern.compile("(?i)lipase");
+    private static final Pattern gelatinasePattern = Pattern.compile("(?i)gelatinase");
+    private static final Pattern polymerasePattern = Pattern.compile("(?i)polymerase");
+    private static final Pattern bccpPattern = Pattern.compile("(?)BCCP-");
+    private static final Pattern bccpPattern2 = Pattern.compile("(?i)-BCCP");
+    private static final Pattern SMURFPattern = Pattern.compile("(?i)SMURF");
+    private static final Pattern ubiquitinPattern = Pattern.compile("(?i)Ubiquitin");
+    private static final Pattern rnasePattern = Pattern.compile("(?i)RNASE");
+    private static final Pattern interleukinPattern = Pattern.compile("(?i)interleukin");
+    private static final Pattern semenogelinPattern = Pattern.compile("(?i)semenogel+in");
+    private static final Pattern synaptotagminPattern = Pattern.compile("(?i)synaptotagmin");
+    private static final Pattern antigenPattern = Pattern.compile("(?i)antigen");
+    private static final Pattern reninPattern = Pattern.compile("(?i)^Renin");
+    private static final Pattern filaggrinPattern = Pattern.compile("(?i)fil+ag+rin");
+    private static final Pattern keratinPattern = Pattern.compile("(?i)keratin");
+    private static final Pattern stromelysinPattern = Pattern.compile("(?i)stromelysin");
+    private static final Pattern caveolinPattern = Pattern.compile("(?i)caveolin");
+    
+    
     
     public static boolean isProteinName(String name) {
         if (kinasePattern.matcher(name).find()) {
@@ -128,6 +155,60 @@ public class ProteinSmallMoleculeDecider {
             return true;
         } else if (acpPattern.matcher(name).find()) {
             return true;
+        } else if (acpPattern2.matcher(name).find()) {
+            return true;
+        } else if (phosphorylasePattern.matcher(name).find()) {
+            return true;
+        } else if (domainsEndingPattern.matcher(name).find()) {
+            return true;
+        } else if (angiotensinPattern.matcher(name).find()) {
+            return true;    
+        } else if (cadherinPattern.matcher(name).find()) {
+            return true;        
+        } else if (cohesinPattern.matcher(name).find()) {
+            return true;        
+        } else if (transporterPattern.matcher(name).find()) {
+            return true;
+        } else if (ligasePattern.matcher(name).find()) {
+            return true;    
+        } else if (polymerasePattern.matcher(name).find()) {
+            return true;    
+        } else if (lipasePattern.matcher(name).find()) {
+            return true;    
+        } else if (gelatinasePattern.matcher(name).find()) {
+            return true;    
+        } else if (bccpPattern.matcher(name).find()) {
+            return true;    
+        } else if (SMURFPattern.matcher(name).find()) {
+            return true;    
+        } else if (acpPattern3.matcher(name).find()) {
+            return true;    
+        } else if (ubiquitinPattern.matcher(name).find()) {
+            return true;    
+        } else if (rnasePattern.matcher(name).find()) {
+            return true;    
+        } else if (interleukinPattern.matcher(name).find()) {
+            return true;    
+        } else if (semenogelinPattern.matcher(name).find()) {
+            return true;    
+        } else if (synaptotagminPattern.matcher(name).find()) {
+            return true;    
+        } else if (bccpPattern2.matcher(name).find()) {
+            return true;    
+        } else if (antigenPattern.matcher(name).find()) {
+            return true;    
+        } else if (reninPattern.matcher(name).find()) {
+            return true;    
+        } else if (filaggrinPattern.matcher(name).find()) {
+            return true;    
+        } else if (paxilinPattern.matcher(name).find()) {
+            return true;    
+        } else if (keratinPattern.matcher(name).find()) {
+            return true;    
+        } else if (stromelysinPattern.matcher(name).find()) {
+            return true;    
+        } else if (caveolinPattern.matcher(name).find()) {
+            return true;    
         } else if (synthasePattern.matcher(name).find()) {
             return true;
         } else if (glycogeninPattern.matcher(name).find()) {
@@ -138,8 +219,6 @@ public class ProteinSmallMoleculeDecider {
         } else if (globinPattern.matcher(name).find()) {
             return true;
         } else if (myosinPattern.matcher(name).find()) {
-            return true;
-        } else if (paxilinPattern.matcher(name).find()) {
             return true;
         } else if (gelsolinPattern.matcher(name).find()) {
             return true;
