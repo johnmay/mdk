406f7dfdd0ccba8a0e547bdf34b36ed68a711e64
John May <johnmay@ebi.ac.uk>
2012-02-10 08:29:47 +0000

    Added MIRIAMIdentifier annotation that allows specification of the MIRIAM MIR in the class source code
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1422 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/Resource/src/main/java/uk/ac/ebi/resource/MIRIAMIdentifier.java
@@ -0,0 +1,40 @@
+/**
+ * IdentifierLoader.java
+ *
+ * 2011.09.15
+ *
+ * This file is part of the CheMet library
+ *
+ * The CheMet library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CheMet is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package uk.ac.ebi.resource;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+/**
+ * 
+ * Annotation type defines the MIRIAM MIR Identifying
+ * a particular resource. ChEBI for example is MIR:00000002
+ * and so a ChEBI identifier is annotated {@code @MIR(id=2)}
+ * 
+ * @author johnmay
+ * 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MIRIAMIdentifier {
+
+    int mir() default 0;
+}
