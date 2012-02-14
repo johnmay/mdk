333aa4fb4fc358b91ce7e233b54730d80f0e461a
John May <johnmay@ebi.ac.uk>
2012-02-14 09:36:24 +0000

    License formating
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1454 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/annotation/src/main/java/uk/ac/ebi/annotation/task/ExecutableParameter.java
+++ CheMet/annotation/src/main/java/uk/ac/ebi/annotation/task/ExecutableParameter.java
@@ -1,31 +1,27 @@
 /**
- * ExecutableParameter.java
- *
- * 2011.10.13
- *
  * This file is part of the CheMet library
- * 
+ *
  * The CheMet library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * CheMet is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * CheMet is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
+
 package uk.ac.ebi.annotation.task;
 
 import java.io.File;
 import java.security.InvalidParameterException;
-import org.apache.log4j.Logger;
 import uk.ac.ebi.interfaces.annotation.Context;
 import uk.ac.ebi.interfaces.annotation.Descriptor;
-import uk.ac.ebi.interfaces.entities.Reaction;
 
 
 /**
