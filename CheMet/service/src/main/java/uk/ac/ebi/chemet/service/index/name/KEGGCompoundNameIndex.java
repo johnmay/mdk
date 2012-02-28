e9a47025c659036c20a63a47315f2ff1afbde7d5
John May <johnmay@ebi.ac.uk>
2012-02-28 10:32:47 +0000

    Added index for KEGG Compound name
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1607 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/service/src/main/java/uk/ac/ebi/chemet/service/index/name/KEGGCompoundNameIndex.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.chemet.service.index.name;
+
+import uk.ac.ebi.chemet.service.index.KeywordNIOIndex;
+
+/**
+ * HMDBNameIndex - 21.02.2012 <br/> Description...
+ *
+ * Index is used in KEGGCompoundLoader
+ *
+ * @author johnmay
+ * @author $Author$ (this version)
+ * @version $Rev$
+ */
+public class KEGGCompoundNameIndex extends KeywordNIOIndex {
+    public KEGGCompoundNameIndex(){
+        super("KEGG Compound Names", "name/kegg-compound");
+    }
+}
