1b6b71aa39177a53205f12498d8bb532d517c45a
John May <johnmay@ebi.ac.uk>
2012-02-29 08:34:29 +0000

    Create new structure service interface
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1624 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/interface/src/main/java/uk/ac/ebi/service/query/StructureService.java
@@ -0,0 +1,22 @@
+package uk.ac.ebi.service.query;
+
+import org.apache.lucene.index.Term;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import uk.ac.ebi.interfaces.identifiers.Identifier;
+
+/**
+ * StructureService - 28.02.2012 <br/>
+ * <p/>
+ * Class descriptions.
+ *
+ * @author johnmay
+ * @author $Author$ (this version)
+ * @version $Rev$
+ */
+public interface StructureService<I extends Identifier> extends QueryService<I>{
+
+    public static final Term ATOM_CONTAINER = new Term("AtomContainer");
+
+    public IAtomContainer getStructure(I identifier);
+
+}
