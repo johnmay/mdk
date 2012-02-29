fe3247a3ce44bd53e08ec1466c583e1eacd12fae
John May <johnmay@ebi.ac.uk>
2012-02-29 08:39:26 +0000

    Added ChEBI Data Service to access charge/formula
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1633 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/service/src/main/java/uk/ac/ebi/chemet/service/query/data/ChEBIDataService.java
@@ -0,0 +1,99 @@
+package uk.ac.ebi.chemet.service.query.data;
+
+import org.apache.log4j.Logger;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.interfaces.IMolecularFormula;
+import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
+import uk.ac.ebi.chemet.service.index.data.ChEBIDataIndex;
+import uk.ac.ebi.chemet.service.index.data.HMDBDataIndex;
+import uk.ac.ebi.chemet.service.loader.writer.DefaultDataIndexWriter;
+import uk.ac.ebi.chemet.service.query.AbstractQueryService;
+import uk.ac.ebi.resource.chemical.ChEBIIdentifier;
+import uk.ac.ebi.resource.chemical.HMDBIdentifier;
+import uk.ac.ebi.service.query.data.MolecularChargeService;
+import uk.ac.ebi.service.query.data.MolecularFormulaService;
+
+import java.util.Collection;
+
+/**
+ * HMDBChemicalDataService - 27.02.2012 <br/>
+ * <p/>
+ * Class provides MolecularChargeService and MolecularFormulaService for the
+ * Human Metabolome Database
+ *
+ * @author johnmay
+ * @author $Author$ (this version)
+ * @version $Rev$
+ */
+public class ChEBIDataService
+        extends AbstractQueryService<ChEBIIdentifier>
+        implements MolecularChargeService<ChEBIIdentifier>,
+                   MolecularFormulaService<ChEBIIdentifier> {
+
+    private static final Logger LOGGER = Logger.getLogger(ChEBIDataService.class);
+
+    private static final IChemObjectBuilder BUILDER = DefaultChemObjectBuilder.getInstance();
+
+    public ChEBIDataService() {
+        super(new ChEBIDataIndex());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public IMolecularFormula getIMolecularFormula(ChEBIIdentifier identifier) {
+        String formula = getMolecularFormula(identifier);
+        if(formula.isEmpty())
+            return BUILDER.newInstance(IMolecularFormula.class);
+        return MolecularFormulaManipulator.getMolecularFormula(formula,
+                                                               BUILDER);
+    }
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public String getMolecularFormula(ChEBIIdentifier identifier) {
+        return getFirstValue(identifier, MOLECULAR_FORMULA);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public Collection<ChEBIIdentifier> searchMolecularFormula(IMolecularFormula formula, boolean approximate) {
+        return searchMolecularFormula(MolecularFormulaManipulator.getString(formula), approximate);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public Collection<ChEBIIdentifier> searchMolecularFormula(String formula, boolean approximate) {
+        if(approximate){
+            LOGGER.error("Approximate search is not yet implemented");
+        }
+        return getIdentifiers(create(formula, MOLECULAR_FORMULA));
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public Double getCharge(ChEBIIdentifier identifier) {
+        String value = getFirstValue(identifier, MOLECULAR_CHARGE);
+        // reuse method for the write that will get a Double.NAN on fail
+        return DefaultDataIndexWriter.getChargeValue(value);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public ChEBIIdentifier getIdentifier() {
+        return new ChEBIIdentifier();
+    }
+
+
+}
