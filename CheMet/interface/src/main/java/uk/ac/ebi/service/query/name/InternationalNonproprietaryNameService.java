a15a8738be9e677fbf57b076608d9139ee4e817f
John May <johnmay@ebi.ac.uk>
2012-02-29 10:10:43 +0000

    Added services for INN and Brand Name
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1643 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/interface/src/main/java/uk/ac/ebi/service/query/name/InternationalNonproprietaryNameService.java
@@ -0,0 +1,46 @@
+package uk.ac.ebi.service.query.name;
+
+import org.apache.lucene.index.Term;
+import uk.ac.ebi.interfaces.identifiers.Identifier;
+import uk.ac.ebi.service.query.QueryService;
+
+import java.util.Collection;
+
+/**
+ * InternationalNonproprietaryNameService - 29.02.2012 <br/>
+ * <p/>
+ * Service provides search and lookup of International Nonpropietrary Names (INN).
+ *
+ * @author johnmay
+ * @author $Author$ (this version)
+ * @version $Rev$
+ */
+public interface InternationalNonproprietaryNameService<I extends Identifier>
+        extends QueryService<I> {
+
+    public static final Term INN = new Term("INN");
+    
+    /**
+     * Access the International Nonpropietrary Name (INN) for a provided
+     * identifier
+     *
+     * @param identifier query to look-up
+     *
+     * @return the INN or empty string
+     */
+    public String getINN(I identifier);
+
+
+    /**
+     * Collection of identifiers that match the International Nonpropietrary
+     * Name (INN). If no matches are found an empty collection is returned
+     *
+     * @param name        the name to search for
+     * @param approximate loosens search criteria
+     *
+     * @return collection of identifiers that have some degree of match with
+     *         the provided name
+     */
+    public Collection<I> searchINN(String name, boolean approximate);
+
+}
