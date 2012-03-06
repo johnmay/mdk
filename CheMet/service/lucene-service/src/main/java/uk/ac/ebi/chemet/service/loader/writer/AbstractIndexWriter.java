732976355362c6e7df12d439becb090f715bfa82
John May <johnmay@ebi.ac.uk>
2012-03-06 14:35:56 +0000

    Create an AbstractWriter that simplifies higher level index writers
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1732 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/service/lucene-service/src/main/java/uk/ac/ebi/chemet/service/loader/writer/AbstractIndexWriter.java
@@ -0,0 +1,107 @@
+package uk.ac.ebi.chemet.service.loader.writer;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Fieldable;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.util.Version;
+import uk.ac.ebi.service.index.LuceneIndex;
+
+import java.io.IOException;
+
+/**
+ * AbstractIndexWriter - 06.03.2012 <br/>
+ * <p/>
+ * Provides basic functionality for an index writer. An instance will create a new {@see IndexWriter}
+ * for the provide {@see LucenIndex}. Documents are added to the index via the {@see add(Document)}
+ * method. You can close the {@see IndexWriter} by invoking {@see close()}.
+ *
+ * @author johnmay
+ * @author $Author$ (this version)
+ * @version $Rev$
+ */
+public class AbstractIndexWriter {
+
+    private IndexWriter writer;
+
+    /**
+     * Create a new AbstractIndexWriter with the directory and anlayzer
+     * provided by the {@see LuceneIndex}
+     *
+     * @param index storage of the directory and analyzer
+     *
+     * @throws IOException low level io error
+     */
+    public AbstractIndexWriter(LuceneIndex index) throws IOException {
+        writer = new IndexWriter(index.getDirectory(),
+                                 new IndexWriterConfig(Version.LUCENE_34,
+                                                       index.getAnalyzer()));
+    }
+
+    /**
+     * Utility method for stored and indexed lucene field creation. This method allows
+     * you to specified the field name with a term. The field is stored (Field.Store.YES)
+     * and index with (Field.Index.ANALYZED)
+     *
+     * @param term  term for the field you want to create
+     * @param value string value to store
+     *
+     * @return instance of new field
+     */
+    public Fieldable create(Term term, String value) {
+        return new Field(term.field(), value, Field.Store.YES, Field.Index.ANALYZED);
+    }
+
+    /**
+     * Utility method for indexed lucene field creation. This method allows you to specified
+     * the field name with a term. The field is index with (Field.Index.ANALYZED)
+     *
+     * @param term  term for the field you want to create
+     * @param value string value to store
+     * @param store whether to store the value
+     *
+     * @return instance of new field
+     */
+    public Fieldable create(Term term, String value, Field.Store store) {
+        return new Field(term.field(), value, store, Field.Index.ANALYZED);
+    }
+
+    /**
+     * Utility method for lucene field creation. This method allows you to specified
+     * the field name with a term.
+     *
+     * @param term    term for the field you want to create
+     * @param value   string value to store
+     * @param store   whether to store the value
+     * @param indexed whether to index the value
+     *
+     * @return instance of new field
+     */
+    public Fieldable create(Term term, String value, Field.Store store, Field.Index indexed) {
+        return new Field(term.field(), value, store, indexed);
+    }
+
+    /**
+     * Add a document to the {@see IndexWriter}. This method wraps
+     * a call to {@see IndexWriter#addDocument(Document)};
+     *
+     * @param document the document to add
+     *
+     * @throws IOException if there is a low-level IO error
+     */
+    public void add(Document document) throws IOException {
+        writer.addDocument(document);
+    }
+
+    /**
+     * Close the underlying index writer
+     *
+     * @throws IOException low-level IO error
+     */
+    public void close() throws IOException {
+        writer.close();
+    }
+
+}
