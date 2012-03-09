30ea304ddae2397a76501bb705d2592813cfd249
John May <johnmay@ebi.ac.uk>
2012-03-09 22:56:04 +0000

    OBA now extends annotation and added generic base type annotation interfaces
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1763 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/interface/src/main/java/uk/ac/ebi/interfaces/annotation/DoubleAnnotation.java
+++ CheMet/interface/src/main/java/uk/ac/ebi/interfaces/annotation/DoubleAnnotation.java
@@ -1,55 +1,38 @@
 /**
- * ObservationBasedAnnotation.java
+ * BasicStringAnnotation.java
  *
- * 2011.12.12
+ * 2011.12.08
  *
  * This file is part of the CheMet library
- *
+ * 
  * The CheMet library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * CheMet is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.ac.ebi.interfaces.annotation;
 
-import java.util.Collection;
-
 import uk.ac.ebi.interfaces.Annotation;
-import uk.ac.ebi.interfaces.Observation;
 
 /**
- *          ObservationBasedAnnotation - 2011.12.12 <br>
- *          Interface describes an annotation that can be based upon
- *          one or more observations
+ *          BasicStringAnnotation - 2011.12.08 <br>
+ *          Interface specifies that annotation can be parsed as a simple string (simplifying editing and IO operations)
  * @version $Rev$ : Last Changed $Date$
  * @author  johnmay
  * @author  $Author$ (this version)
  */
-public interface ObservationBasedAnnotation<O extends Observation> extends Annotation {
+public interface DoubleAnnotation extends Annotation {
 
-    /**
-     * Access a collection of observations supporting this annotation
-     * @return
-     */
-    public Collection<O> getObservations();
+    public Double getValue();
 
-    /**
-     * An a single observation that supports the annotation (previous
-     * observations are not removed)
-     */
-    public boolean addObservation(O observation);
+    public void setValue(Double value);
 
-    /**
-     * An numerous observation that support the annotation (previous
-     * observations are not removed)
-     */
-    public boolean addObservations(Collection<O> observations);
 }
