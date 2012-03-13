9a5564806c188cb99a27f174340f07fd74a9ac12
John May <johnmay@ebi.ac.uk>
2012-03-13 10:10:18 +0000

    added documentation
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1795 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

--- CheMet/interface/src/main/java/uk/ac/ebi/interfaces/entities/Metabolite.java
+++ CheMet/interface/src/main/java/uk/ac/ebi/interfaces/entities/Metabolite.java
@@ -5,26 +5,49 @@
 package uk.ac.ebi.interfaces.entities;
 
 import java.util.Collection;
+
 import uk.ac.ebi.interfaces.MetaboliteClass;
 import uk.ac.ebi.interfaces.AnnotatedEntity;
 import uk.ac.ebi.interfaces.annotation.ChemicalStructure;
 
 
 /**
+ * Description of a metabolic instance.
  *
  * @author johnmay
  */
 public interface Metabolite extends AnnotatedEntity {
 
+    /**
+     * Access the charge of this metabolite. If no charge
+     * has been added this should return {@see Double#NAN}
+     *
+     * @return charge value for the metabolite of NAN if
+     *         not set
+     */
     public Double getCharge();
 
-
+    /**
+     * Set the charge on the metabolite
+     *
+     * @param charge
+     */
     public void setCharge(Double charge);
 
+    /**
+     * Set whether the molecule is generic. A generic metabolite
+     * is much like an abstract class. It provides some information
+     * on the metabolite but is not concrete.
+     *
+     * @param generic if the metabolic is generic
+     */
+    public void setGeneric(boolean generic);
 
-    public void setGeneric(boolean readBoolean);
-
-
+    /**
+     * Access whether the metabolite is generic
+     *
+     * @return
+     */
     public boolean isGeneric();
 
 
@@ -34,15 +57,21 @@ public interface Metabolite extends AnnotatedEntity {
     public Enum<? extends MetaboliteClass> getType();
 
     /**
-     * Method replaces hasStructureAssociated to be more direct
-     * @return 
+     * Method determines whether this metabolite has a
+     * chemical structure annotation (see. chemet-annotation)
+     * module for types of annotations.
+     *
+     * @return whether there is structure for this metabolite
      */
     public boolean hasStructure();
-    
+
     /**
-     * Convenience method for accessing all associated chemical structures
-     * @return 
+     * Convenience method for accessing all associated chemical
+     * structures. If no chemical structure is present on the
+     * metabolite an empty collection is returned.
+     *
+     * @return collection of {@see ChemicalStructure} annotations
      */
     public Collection<ChemicalStructure> getStructures();
-    
+
 }
