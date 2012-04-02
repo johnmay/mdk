e5d8778bb21620b0ba808463af760ac3c88d580c
John May <johnmay@ebi.ac.uk>
2012-04-02 09:02:40 +0000

    Added test that reader/writes have @CompatibleSince annotation for annotation and observation data io
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1906 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/io/annotation-data-io/src/test/java/uk/ac/ebi/chemet/io/annotation/AnnotationDataOutputStreamTest.java
@@ -0,0 +1,34 @@
+package uk.ac.ebi.chemet.io.annotation;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import uk.ac.ebi.caf.utility.version.Version;
+import uk.ac.ebi.caf.utility.version.VersionMap;
+import uk.ac.ebi.caf.utility.version.annotation.CompatibleSince;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author johnmay
+ * @author $Author$ (this version)
+ * @version $Rev$
+ */
+public class AnnotationDataOutputStreamTest {
+
+    @Test
+    public void testVersionTagsExist() throws IOException {
+        AnnotationDataOutputStream manager = new AnnotationDataOutputStream(new DataOutputStream(new ByteArrayOutputStream()),
+                                                                            new Version("1.0"));
+        for(VersionMap map : manager.getMarshals().values()){
+            for(Object o : map.values()){
+                assertNotNull(o.getClass().getAnnotation(CompatibleSince.class));
+            }
+        }
+
+    }
+
+}
---
+++ CheMet/io/observation-data-io/src/test/java/uk/ac/ebi/chemet/io/observation/ObservationDataOutputStreamTest.java
@@ -0,0 +1,33 @@
+package uk.ac.ebi.chemet.io.observation;
+
+import org.junit.Test;
+import uk.ac.ebi.caf.utility.version.Version;
+import uk.ac.ebi.caf.utility.version.VersionMap;
+import uk.ac.ebi.caf.utility.version.annotation.CompatibleSince;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author johnmay
+ * @author $Author$ (this version)
+ * @version $Rev$
+ */
+public class ObservationDataOutputStreamTest {
+
+    @Test
+    public void testVersionTagsExist() throws IOException {
+        ObservationDataOutputStream manager = new ObservationDataOutputStream(new DataOutputStream(new ByteArrayOutputStream()),
+                                                                            new Version("1.0"));
+        for(VersionMap map : manager.getMarshals().values()){
+            for(Object o : map.values()){
+                assertNotNull(o.getClass().getAnnotation(CompatibleSince.class));
+            }
+        }
+
+    }
+
+}
