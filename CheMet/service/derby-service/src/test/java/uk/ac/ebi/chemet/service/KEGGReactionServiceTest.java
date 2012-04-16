fbebc76092c5ff5e7e897ceb8de9d63520d823ca
John May <johnmay@ebi.ac.uk>
2012-04-16 11:46:00 +0000

    Added KEGGReaction service using derby (needs porting to use hibernate)
    git-svn-id: https://svn.ebi.ac.uk/steinbeck-research/CheMet/trunk@1961 47e25a5e-c6eb-4c94-8020-4a4a81ee2334

---
+++ CheMet/service/derby-service/src/test/java/uk/ac/ebi/chemet/service/KEGGReactionServiceTest.java
@@ -0,0 +1,34 @@
+package uk.ac.ebi.chemet.service;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import uk.ac.ebi.chemet.resource.classification.ECNumber;
+import uk.ac.ebi.core.DefaultEntityFactory;
+import uk.ac.ebi.interfaces.entities.EntityFactory;
+
+import java.sql.SQLException;
+
+/**
+ * KEGGReactionServiceTest - 07.03.2012 <br/>
+ * <p/>
+ * Class descriptions.
+ *
+ * @author johnmay
+ * @author $Author$ (this version)
+ * @version $Rev$
+ */
+public class KEGGReactionServiceTest {
+
+    private static final Logger LOGGER = Logger.getLogger(KEGGReactionServiceTest.class);
+
+    @Test
+    public void testService() throws SQLException {
+
+        EntityFactory factory = DefaultEntityFactory.getInstance();
+        KEGGReactionService service = new KEGGReactionService(factory);
+
+        System.out.println(service.getReaction(new ECNumber("1.1.1.85")));
+
+    }
+
+}
