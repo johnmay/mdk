fb9e2c0de8c7aabaca74da6de9ab445cf16e3884
John May <john.wilkinsonmay@gmail.com>
2012-04-25 17:21:13 +0100

    Forcing rename

--- CheMet-Core/src/test/java/uk/ac/ebi/core/tools/compare/CrossreferenceAltComparatorTest.java
+++ CheMet-Core/src/test/java/uk/ac/ebi/core/tools/compare/CrossreferenceAltComparatorTest.java
@@ -13,9 +13,9 @@ import uk.ac.ebi.interfaces.entities.Metabolite;
 /**
  * @author John May
  */
-public class CrossreferenceComparatorTest {
+public class CrossreferenceAltComparatorTest {
 
-    private static final Logger LOGGER = Logger.getLogger(CrossreferenceComparatorTest.class);
+    private static final Logger LOGGER = Logger.getLogger(CrossreferenceAltComparatorTest.class);
 
     private EntityComparator<Metabolite> comparator = new CrossreferenceComparator<Metabolite>();
 
