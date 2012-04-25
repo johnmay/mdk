23dafb0709f1083a14f46c7654c0fc0177ba4d91
John May <john.wilkinsonmay@gmail.com>
2012-04-25 11:55:07 +0100

    Updated StructureIndexWriter to write fingerpint

--- service/lucene-service/src/main/java/uk/ac/ebi/chemet/service/loader/writer/DefaultStructureIndexWriter.java
+++ service/lucene-service/src/main/java/uk/ac/ebi/chemet/service/loader/writer/DefaultStructureIndexWriter.java
@@ -1,19 +1,24 @@
 package uk.ac.ebi.chemet.service.loader.writer;
 
+import org.apache.log4j.Logger;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.util.Version;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.fingerprint.Fingerprinter;
+import org.openscience.cdk.fingerprint.IFingerprinter;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import uk.ac.ebi.chemet.service.analyzer.FingerprintSimilarity;
 import uk.ac.ebi.service.index.LuceneIndex;
-import uk.ac.ebi.service.query.QueryService;
-import uk.ac.ebi.service.query.StructureService;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
+import java.util.BitSet;
+
+import static org.apache.lucene.document.Field.Index.NOT_ANALYZED_NO_NORMS;
+import static org.apache.lucene.document.Field.Store.NO;
+import static uk.ac.ebi.service.query.StructureService.*;
 
 /**
  * ${Name}.java - 20.02.2012 <br/> MetaInfo...
@@ -22,16 +27,20 @@ import java.io.ObjectOutputStream;
  * @author $Author$ (this version)
  * @version $Rev$
  */
-public class DefaultStructureIndexWriter {
+public class DefaultStructureIndexWriter extends AbstractIndexWriter {
 
+    private static final Logger LOGGER = Logger.getLogger(DefaultStructureIndexWriter.class);
 
-    private IndexWriter writer;
+    private IFingerprinter fingerprinter;
 
+    public DefaultStructureIndexWriter(LuceneIndex index, IFingerprinter fingerprinter) throws IOException {
+        super(index);
+        this.fingerprinter = fingerprinter;
+        getWriter().getConfig().setSimilarity(new FingerprintSimilarity());
+    }
 
     public DefaultStructureIndexWriter(LuceneIndex index) throws IOException {
-        writer = new IndexWriter(index.getDirectory(),
-                                 new IndexWriterConfig(Version.LUCENE_34,
-                                                       index.getAnalyzer()));
+        this(index, new Fingerprinter());
     }
 
     /**
@@ -50,25 +59,38 @@ public class DefaultStructureIndexWriter {
         out.close();
 
         // Get the bytes of the serialized object
-        write(identifier, bos.toByteArray());
+        write(identifier, bos.toByteArray(), getFingerprint(molecule));
 
 
+    }
+
+    public BitSet getFingerprint(IAtomContainer molecule){
+        try {
+            return fingerprinter.getFingerprint(molecule);
+        } catch (CDKException ex){
+            LOGGER.warn("Fingerprint was not calculated for molecule: " + molecule );
+            return new BitSet();
+        }
     }
 
     private void write(String identifier,
-                    byte[] molecule) throws IOException {
+                    byte[] molecule,
+                    BitSet fp) throws IOException {
 
         Document document = new Document();
-        document.add(new Field(QueryService.IDENTIFIER.field(),
-                               identifier
-                , Field.Store.NO, Field.Index.ANALYZED));
-        document.add(new Field(StructureService.ATOM_CONTAINER.field(), molecule));
-        writer.addDocument(document);
+
+        // store the identifier and AtomContainer values
+        document.add(create(IDENTIFIER, identifier));
+        document.add(new Field(ATOM_CONTAINER.field(), molecule));
+
+        // index the fingerprint
+        for(int i = fp.nextSetBit(0); i != -1; i = fp.nextSetBit(i + 1)){
+            document.add(create(FINGERPRINT_BIT, Integer.toString(i), NO, NOT_ANALYZED_NO_NORMS));
+        }
+
+        add(document);
 
     }
 
-    public void close() throws IOException {
-        writer.close();
-    }
 
 }
