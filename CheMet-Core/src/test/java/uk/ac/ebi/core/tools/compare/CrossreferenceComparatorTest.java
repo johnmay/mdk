60fedcd8d6c368b8fbcfeafe0c6d43a220794f43
John May <john.wilkinsonmay@gmail.com>
2012-04-25 21:45:53 +0100

    Renaming to proper camel case
    Can't make my mind up on hypens with camel case

--- CheMet-Core/src/test/java/uk/ac/ebi/core/tools/compare/CrossreferenceComparatorTest.java
+++ CheMet-Core/src/test/java/uk/ac/ebi/core/tools/compare/CrossreferenceComparatorTest.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 import uk.ac.ebi.annotation.crossreference.CrossReference;
+import uk.ac.ebi.annotation.crossreference.KEGGCrossReference;
 import uk.ac.ebi.chemet.resource.basic.BasicChemicalIdentifier;
 import uk.ac.ebi.chemet.resource.chemical.KEGGCompoundIdentifier;
 import uk.ac.ebi.core.MetaboliteImplementation;
@@ -13,11 +14,11 @@ import uk.ac.ebi.interfaces.entities.Metabolite;
 /**
  * @author John May
  */
-public class CrossreferenceComparatorTest {
+public class CrossReferenceComparatorTest {
 
-    private static final Logger LOGGER = Logger.getLogger(CrossreferenceComparatorTest.class);
+    private static final Logger LOGGER = Logger.getLogger(CrossReferenceComparatorTest.class);
 
-    private EntityComparator<Metabolite> comparator = new CrossreferenceComparator<Metabolite>();
+    private EntityComparator<Metabolite> comparator = new CrossReferenceComparator<Metabolite>();
 
     @Test
     public void testDirectReference() throws Exception {
@@ -37,6 +38,8 @@ public class CrossreferenceComparatorTest {
 
 
     }
+
+
     @Test
     public void testIndirectReference() throws Exception {
 
@@ -48,6 +51,28 @@ public class CrossreferenceComparatorTest {
 
         m1.addAnnotation(new CrossReference(new KEGGCompoundIdentifier("C00009")));
         m2.addAnnotation(new CrossReference(new KEGGCompoundIdentifier("C00009")));
+
+        Assert.assertTrue(comparator.equal(m1, m2));
+        Assert.assertTrue(comparator.equal(m2, m1)); // simulate transpose
+
+
+    }
+
+    /**
+     * Tests that sub-classes get picked up correctly
+     * @throws Exception
+     */
+    @Test
+    public void testIndirectReference_subclass() throws Exception {
+
+        Metabolite m1 = new MetaboliteImplementation(new BasicChemicalIdentifier("test-1"), "", "");
+        Metabolite m2 = new MetaboliteImplementation(new BasicChemicalIdentifier("test-2"), "", "");
+
+        Assert.assertFalse(comparator.equal(m1, m2));
+        Assert.assertFalse(comparator.equal(m2, m1));
+
+        m1.addAnnotation(new KEGGCrossReference(new KEGGCompoundIdentifier("C00009")));
+        m2.addAnnotation(new KEGGCrossReference(new KEGGCompoundIdentifier("C00009")));
 
         Assert.assertTrue(comparator.equal(m1, m2));
         Assert.assertTrue(comparator.equal(m2, m1)); // simulate transpose
