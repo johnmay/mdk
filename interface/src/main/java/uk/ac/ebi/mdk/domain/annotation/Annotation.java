76ba99e3474dd388472cce7e9d2d5a018b7c7c5c
John May <john.wilkinsonmay@gmail.com>
2012-04-26 13:46:13 +0100

    Updating copyright notice to ensure all files are LGPL v 2.1 +

--- interface/src/main/java/uk/ac/ebi/mdk/domain/annotation/Annotation.java
+++ interface/src/main/java/uk/ac/ebi/mdk/domain/annotation/Annotation.java
@@ -15,6 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
 package uk.ac.ebi.mdk.domain.annotation;
 
 import uk.ac.ebi.mdk.domain.Descriptor;
--- interface/src/main/java/uk/ac/ebi/mdk/domain/entity/collection/EntityCollection.java
+++ interface/src/main/java/uk/ac/ebi/mdk/domain/entity/collection/EntityCollection.java
@@ -1,22 +1,19 @@
-/**
- * SelectionMap.java
+/*
+ * Copyright (C) 2012  John May and Pablo Moreno
  *
- * 2011.10.14
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This file is part of the CheMet library
- * 
- * The CheMet library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * CheMet is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with CheMet.  If not, see <http://www.gnu.org/licenses/>.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 package uk.ac.ebi.mdk.domain.entity.collection;
 
