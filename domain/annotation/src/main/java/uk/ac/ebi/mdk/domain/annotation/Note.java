7a5a4aa6e157245b8ca8f096038442959a1e3a1c
John May <john.wilkinsonmay@gmail.com>
2012-10-12 16:53:09 +0100

    Added new annotation for handling notes with no identifiable author

---
+++ domain/annotation/src/main/java/uk/ac/ebi/mdk/domain/annotation/Note.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2012. John May <jwmay@sf.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.ebi.mdk.domain.annotation;
+
+import uk.ac.ebi.mdk.domain.annotation.primitive.AbstractStringAnnotation;
+import uk.ac.ebi.mdk.domain.annotation.primitive.StringAnnotation;
+import uk.ac.ebi.mdk.lang.annotation.Brief;
+import uk.ac.ebi.mdk.lang.annotation.Context;
+import uk.ac.ebi.mdk.lang.annotation.Description;
+
+/**
+ * A simple string annotation that indicate a note on a given entity. This
+ * annotation is similar to {@link AuthorAnnotation} but does not store any
+ * information about who created the note. Normally this annotation should be
+ * used when importing annotations from external sources and no author is
+ * provided.
+ *
+ * @author John May
+ * @see AuthorAnnotation
+ */
+@Context
+@Brief("Note")
+@Description("A free text note on a metabolic entity")
+public class Note extends AbstractStringAnnotation {
+
+    /**
+     * Default constructor - builds a note with empty content.
+     */
+    public Note() {
+    }
+
+    /**
+     * Construct a node with a provided value
+     * @param content the content of the note
+     */
+    public Note(String content) {
+        super(content);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public Note getInstance(String value) {
+        return new Note(value);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public Note newInstance() {
+        return new Note();
+    }
+}
---
+++ domain/annotation/src/test/java/uk/ac/ebi/mdk/domain/annotation/NoteTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2012. John May <jwmay@sf.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.ebi.mdk.domain.annotation;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author John May
+ */
+public class NoteTest {
+
+    @Test
+    public void testGetInstance() throws Exception {
+
+        Note instance = new Note().getInstance("this is a note");
+
+        Assert.assertNotNull("new instance was null",
+                instance);
+        Assert.assertTrue(   "value was not passed",
+                             instance.getValue().equals("this is a note"));
+    }
+
+    @Test
+    public void testNewInstance() throws Exception {
+
+        Assert.assertNotNull("new instance was null",
+                new Note().newInstance());
+
+    }
+
+    @Test
+    public void testSetGetValue() throws Exception {
+
+        Note note = new Note();
+
+        Assert.assertNotNull("null value", note.getValue());
+        Assert.assertTrue(   "expected empty note", note.getValue().isEmpty());
+
+        note.setValue("new value");
+        Assert.assertEquals("new value", note.getValue());
+
+
+    }
+
+
+    @Test
+    public void testEquals() throws Exception {
+
+        Note n1 = new Note("one");
+        Note n2 = new Note("two");
+        Note n3 = new Note("one");
+
+        Assert.assertNotSame("notes 1 and 2 should not be equal", n1, n2);
+        Assert.assertEquals("notes 1 and 3 should be equal", n1, n3);
+
+
+
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+
+        Note n1 = new Note("one");
+        Note n2 = new Note("two");
+        Note n3 = new Note("one");
+
+        Assert.assertNotSame("notes 1 and 2 hashCode should not be equal", n1.hashCode(), n2.hashCode());
+        Assert.assertEquals("notes 1 and 3 hashCode should be equal", n1.hashCode(), n3.hashCode());
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        Note note = new Note("I note, therefore I am");
+        Assert.assertEquals("I note, therefore I am", note.toString());
+    }
+
+    @Test
+    public void testGetShortDescription() throws Exception {
+        Assert.assertEquals("Note", new Note().getShortDescription());
+    }
+
+    @Test
+    public void testGetBrief() throws Exception {
+        Assert.assertEquals("Note", new Note().getBrief());
+    }
+
+    @Test
+    public void testGetLongDescription() throws Exception {
+        Assert.assertEquals("A free text note on a metabolic entity", new Note().getLongDescription());
+    }
+
+    @Test
+    public void testGetDescription() throws Exception {
+        Assert.assertEquals("A free text note on a metabolic entity", new Note().getDescription());
+    }
+}
