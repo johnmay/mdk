dcd68c26431782838b26f55ce995b8a7a85d4300
John May <john.wilkinsonmay@gmail.com>
2013-03-08 17:30:35 +0000

    correcting license and added unit tests for missing structure predicate

--- tool/domain/src/main/java/uk/ac/ebi/mdk/tool/domain/MissingStructureParticipant.java
+++ tool/domain/src/main/java/uk/ac/ebi/mdk/tool/domain/MissingStructureParticipant.java
@@ -2,19 +2,18 @@
  * Copyright (C) 2013 Pablo Moreno <pablacious at users.sf.net>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package uk.ac.ebi.mdk.tool.domain;
 
 import com.google.common.base.Predicate;
@@ -22,30 +21,29 @@ import uk.ac.ebi.mdk.domain.entity.reaction.MetabolicParticipant;
 import uk.ac.ebi.mdk.domain.entity.reaction.MetabolicReaction;
 
 /**
- * @name    MissingStructureParticipant
- * @date    2013.03.07
- * @version $Rev$ : Last Changed $Date$
- * @author  Pablo Moreno <pablacious at users.sf.net>
- * @author  $Author$ (this version)
- * @brief   Checks whether a MetabolicReaction has participants that do not have a molecule assigned or if the assigned
- *          molecule has no structure.
+ * Checks whether a MetabolicReaction has participants that do not have a
+ * molecule assigned or if the assigned molecule has no structure.
  *
+ * @author Pablo Moreno
  */
-public class MissingStructureParticipant implements Predicate<MetabolicReaction> {
+public class MissingStructureParticipant
+        implements Predicate<MetabolicReaction> {
 
     /**
-     * Returns true if the given MetabolicReaction has at least one participant which doesn't have any chemical 
-     * structure.
-     * 
-     * @param reaction the reaction to check for participants without structure.
+     * Returns true if the given MetabolicReaction has at least one participant
+     * which doesn't have any chemical structure.
+     *
+     * @param reaction the reaction to check for participants without
+     *                 structure.
      * @return true if reaction has participant with no structure.
      */
     @Override
     public boolean apply(MetabolicReaction reaction) {
         for (MetabolicParticipant metabolicParticipant : reaction.getParticipants()) {
-            if(metabolicParticipant.getMolecule()==null || !metabolicParticipant.getMolecule().hasStructure()) {
+            if (metabolicParticipant.getMolecule() == null
+                    || !metabolicParticipant.getMolecule().hasStructure()) {
                 return true;
-            }   
+            }
         }
         return false;
     }
