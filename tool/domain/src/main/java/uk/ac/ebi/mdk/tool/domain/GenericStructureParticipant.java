323826a29d0f1c57d7e352cfb6b4a44d348d1b34
John May <john.wilkinsonmay@gmail.com>
2013-03-08 18:12:25 +0000

    updating license and unit test for GenericStructureParticipant

--- tool/domain/src/main/java/uk/ac/ebi/mdk/tool/domain/GenericStructureParticipant.java
+++ tool/domain/src/main/java/uk/ac/ebi/mdk/tool/domain/GenericStructureParticipant.java
@@ -2,16 +2,16 @@
  * Copyright (C) 2013 Pablo Moreno <pablacious at users.sf.net>
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
@@ -22,26 +22,24 @@ import uk.ac.ebi.mdk.domain.entity.reaction.MetabolicParticipant;
 import uk.ac.ebi.mdk.domain.entity.reaction.MetabolicReaction;
 
 /**
- * @name    GenericStructureParticipant
- * @date    2013.03.07
- * @version $Rev$ : Last Changed $Date$
- * @author  Pablo Moreno <pablacious at users.sf.net>
- * @author  $Author$ (this version)
- * @brief   Checks whether a MetabolicReaction has generic molecule participants.
+ * Checks whether a MetabolicReaction has generic molecule participants.
  *
+ * @author Pablo Moreno
  */
-public class GenericStructureParticipant implements Predicate<MetabolicReaction>{
+public class GenericStructureParticipant
+        implements Predicate<MetabolicReaction> {
 
     /**
-     * Returns true if the given reaction has at least one molecule with generic structure among its participants.
-     * 
+     * Returns true if the given reaction has at least one molecule with generic
+     * structure among its participants.
+     *
      * @param reaction the reaction to check for generic participants.
      * @return true if at least one participant is generic.
      */
     @Override
     public boolean apply(MetabolicReaction reaction) {
-        for (MetabolicParticipant metabolicParticipant : reaction.getParticipants()) {
-            if(metabolicParticipant.getMolecule().isGeneric()) {
+        for (MetabolicParticipant p : reaction.getParticipants()) {
+            if (p.getMolecule() != null && p.getMolecule().isGeneric()) {
                 return true;
             }
         }
