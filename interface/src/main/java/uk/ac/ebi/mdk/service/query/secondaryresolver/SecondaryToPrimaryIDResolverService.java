26def13d30591fe630526a7d81d812491b40ea30
Pablo <pablo.a.moreno@gmail.com>
2013-03-12 10:50:47 +0000

    Adds terms for Primary and Secondary Identifiers to the interface. Objects implementing this interface are currently outside mdi, but they rely on this interface.

--- interface/src/main/java/uk/ac/ebi/mdk/service/query/secondaryresolver/SecondaryToPrimaryIDResolverService.java
+++ interface/src/main/java/uk/ac/ebi/mdk/service/query/secondaryresolver/SecondaryToPrimaryIDResolverService.java
@@ -1,10 +1,23 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2013 Pablo Moreno <pablacious at users.sf.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.ac.ebi.mdk.service.query.secondaryresolver;
 
 import java.util.Collection;
+import org.apache.lucene.index.Term;
 import uk.ac.ebi.mdk.domain.identifier.Identifier;
 import uk.ac.ebi.mdk.service.query.QueryService;
 
@@ -14,6 +27,8 @@ import uk.ac.ebi.mdk.service.query.QueryService;
  */
 public interface SecondaryToPrimaryIDResolverService<I extends Identifier> extends QueryService<I> {
 
+    public static final Term PRIMARY_IDENTIFIER = new Term("PrimaryID");
+    public static final Term SECONDARY_IDENTIFIER = new Term("SecondaryID");
     /**
      * The method takes an identifier, presumed to be a secondary Identifier of the database, looks for a primary id associated in the
      * index and returns it. If the identifier is a primary identifier, the same identifier is returned.
