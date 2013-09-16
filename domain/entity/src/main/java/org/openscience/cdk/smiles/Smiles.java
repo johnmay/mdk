fff0c9c5919ac63f0ccb6db8f0b498e42ce0a12f
John May <john.wilkinsonmay@gmail.com>
2013-09-16 12:55:18 +0100

    Use custom CDK fork with SMILES and MDL patches - this class can be removed once CDK is up to date.

---
+++ domain/entity/src/main/java/org/openscience/cdk/smiles/Smiles.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2013. EMBL, European Bioinformatics Institute
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.openscience.cdk.smiles;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import uk.ac.ebi.beam.Functions;
+import uk.ac.ebi.beam.Graph;
+
+/**
+ * Utility for reading and writing SMILES until the CDK is patched with Beam.
+ *
+ * @author John May
+ */
+public final class Smiles {
+
+    private static final IChemObjectBuilder builder  = SilentChemObjectBuilder.getInstance();
+    private static final BeamToCDK          fromBeam = new BeamToCDK(builder);
+    private static final CDKToBeam          toBeam   = new CDKToBeam();
+
+    /**
+     * Read the molecule from the SMILES string - if the string could not be
+     * read (invalid SMILES) and empty molecule is returned.
+     *
+     * @param smi SMILES string
+     * @return a molecule (empty if a parse exception occurred)
+     */
+    public static IAtomContainer read(String smi) {
+        try {
+            Graph g = Graph.fromSmiles(smi);
+            return fromBeam.toAtomContainer(g);
+        } catch (Exception e) {
+            return builder.newInstance(IAtomContainer.class, 0, 0, 0, 0);
+        }
+    }
+
+    /**
+     * Read the molecule from the SMILES string - if the string could not be
+     * generated an empty string is returned.
+     *
+     * @param m molecule
+     * @return a molecule (empty if a parse exception occurred)
+     */
+    public static String write(IAtomContainer m) {
+        try {
+            return Functions.collapse(toBeam.toBeamGraph(m)).toSmiles();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
---
+++ domain/entity/src/test/java/org/openscience/cdk/smiles/SmilesTest.java
@@ -0,0 +1,37 @@
+package org.openscience.cdk.smiles;
+
+import org.junit.Test;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IBond;
+import org.openscience.cdk.silent.Atom;
+import org.openscience.cdk.silent.AtomContainer;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/** @author John May */
+public class SmilesTest {
+
+    @Test
+    public void read_benzene() throws Exception {
+        IAtomContainer m = Smiles.read("C1=CC=CC=C1");
+        assertThat(m.getAtomCount(), is(6));
+    }
+
+    @Test
+    public void read_invalid() throws Exception {
+        IAtomContainer m = Smiles.read("CCC[ACP]");
+        assertThat(m.getAtomCount(), is(0));
+    }
+
+    @Test
+    public void write_ethene() throws Exception {
+        IAtomContainer m = new AtomContainer();
+        m.addAtom(new Atom("C"));        
+        m.addAtom(new Atom("C"));
+        m.getAtom(0).setImplicitHydrogenCount(2);
+        m.getAtom(1).setImplicitHydrogenCount(2);
+        m.addBond(0, 1, IBond.Order.DOUBLE);
+        assertThat(Smiles.write(m), is("C=C"));
+    }
+}
